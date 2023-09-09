0ebaf69154459fc2c096f2edc57d6b93324548e5
garrettsiegel <garrett@submurgedcreative.com>
2023-09-09 14:24:43 -0400

    Update Google Analytics

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,16 +3,12 @@ import AnimationSkills from "./animationSkills"
 import Modal from "./modal"
 import LazyLoad from "./lazyLoad"
 
-import Car from "./newClass"
-
 class App {
 	constructor() {
 		new IntroButton()
     new AnimationSkills()
 		new Modal()
 		new LazyLoad()
-
-		new Car()
 	}
 
 }
